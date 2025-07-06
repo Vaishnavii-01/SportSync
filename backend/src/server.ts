c4444045c0202cfbeffdc3cf200d7247ed4a544b
Harshita Singh <harshitaks2005@gmail.com>
2025-07-06 22:00:15 +0530

    basic slot visbility

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,15 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import connectDB from "./config/db";
-import authRoutes from "./routes/authRoutes";
-import venueRoutes from "./routes/venueRoutes";
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/db';
+import authRoutes from './routes/authRoutes';
+import venueRoutes from './routes/venueRoutes';
+
+import sectionRoutes from './routes/sectionRoutes'
+import slotRoutes from './routes/slotRoutes';
+
+
+
 dotenv.config();
 const app = express();
 
@@ -17,8 +23,11 @@ app.use(express.json());
 
 connectDB();
 
-app.use("/api/auth", authRoutes);
-app.use("/api/venues", venueRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/venues', venueRoutes); 
+app.use('/api/sections', sectionRoutes);
+app.use('/api/slots', slotRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
